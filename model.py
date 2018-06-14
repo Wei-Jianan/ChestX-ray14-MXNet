ff7850b4096a7afe934fd563542fd2dcf33f2a07
Wei-Jianan <weij@TCD.ie>
2018-06-14 15:02:20 +0100

    forget the context specified

--- model.py
+++ model.py
@@ -2,11 +2,11 @@ from mxnet import gluon, init, nd
 from mxnet.gluon import nn
 
 
-def get_pretrained_resnet50():
-    pretrained_net = gluon.model_zoo.vision.resnet50_v1(pretrained=True)
+def get_pretrained_resnet50(ctx):
+    pretrained_net = gluon.model_zoo.vision.resnet50_v1(pretrained=True, ctx=ctx)
     # temp_data = nd.random_normal(shape=(1, 4096))
     output_layer = nn.Dense(14, weight_initializer=init.Xavier())
-    output_layer.initialize()
+    output_layer.initialize(ctx=ctx)
 
     tuning_net = nn.HybridSequential()
     with tuning_net.name_scope():
